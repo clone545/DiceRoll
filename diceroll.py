0d18d121c2b1071667c53de2cfcc52e76602281e
Micro <hello@internet.com>
2016-10-31 12:11:46 +0000

    Added custom number of dice sides.

--- diceroll.py
+++ diceroll.py
@@ -2,14 +2,14 @@
 
 from random import randint
 
-def roll(times):
+def roll(sides, times):
     if times == 0:
         print("You can't not roll the dice!")
         return False
 
     while times > 0:
-        print("You rolled a %s" % randint(1,6))
+        print("You rolled a %s" % randint(1,sides))
         times -= 1
     return roll
 
-roll(int(input("How many die need to be cast?: ")))
+roll(int(input("How many sides does your dice have?: ")),int(input("How many die need to be cast?: ")))
